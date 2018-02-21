eeddd98720b38e644bc1ac5eca23f1baa3432c40
almudza <alifulmudzakir@gmail.com>
2018-02-21 13:02:41 +0700

    fixed nav-brand

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}" >
 
-        <title>Larricles</title>
+        <title>Larticles</title>
         
         <script>window.Laravel = { csrfToken: '{{ csrf_token() }}'}</script>
 
